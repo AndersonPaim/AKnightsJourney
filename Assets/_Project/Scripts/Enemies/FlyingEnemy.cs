d3180352bc23e962c01c3ea018af1becc56d6ae8
Anderson Paim <andersonpaim15@outlook.com>
2022-04-17 23:11:22 -0300

    Started flying enemy

---
+++ Assets/_Project/Scripts/Enemies/FlyingEnemy.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour, IDamageable
+{
+    [SerializeField] private GameObject _projectileObj;
+    [SerializeField] private List<Transform> _projectilePosList;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private Animator _anim;
+    [SerializeField] private Collider _hitCollider;
+    [SerializeField] private float _shootDelay;
+    [SerializeField] private float _shootForce;
+    [SerializeField] private float _health;
+
+    private bool _isDead = false;
+
+    public void TakeDamage(float damage, GameObject attacker)
+    {
+        _health -= damage;
+
+        if(_health > 0)
+        {
+            _anim.SetTrigger("TakeDamage");
+        }
+        else
+        {
+            _anim.SetTrigger("Die");
+            StartCoroutine(DestroyDelay(2));
+            GetComponent<Collider>().enabled = false;
+            _hitCollider.enabled = false;
+            _isDead = true;
+            _rb.useGravity = true;
+            _rb.isKinematic = true;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    private IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(_shootDelay);
+
+        _anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(0.4f); //sync shoot with animation
+
+        if(!_isDead)
+        {
+            foreach(Transform pos in _projectilePosList)
+            {
+                GameObject obj = Instantiate(_projectileObj, pos.position, pos.rotation);
+                Rigidbody rb = obj.GetComponent<Rigidbody>();
+                rb.AddRelativeForce(Vector3.forward * _shootForce);
+            }
+
+            StartCoroutine(Shoot());
+         }
+    }
+
+    private IEnumerator DestroyDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
